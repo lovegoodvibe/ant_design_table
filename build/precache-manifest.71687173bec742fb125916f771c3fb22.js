1b63b568f55903b642ddf3f3ee8f0a1e3fffd7da
tunguyen <nst30012001@gmail.com>
2022-09-10 20:59:11 +0700

    add build

---
+++ build/precache-manifest.71687173bec742fb125916f771c3fb22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba49721168b6a7bc76e4",
+    "url": "/static/js/main.ba497211.chunk.js"
+  },
+  {
+    "revision": "b24ed5a13f9e58379c52",
+    "url": "/static/css/1.485f6c5a.chunk.css"
+  },
+  {
+    "revision": "b24ed5a13f9e58379c52",
+    "url": "/static/js/1.b24ed5a1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b2596a33a9746ebccf5f40891f65d5e",
+    "url": "/index.html"
+  }
+];
